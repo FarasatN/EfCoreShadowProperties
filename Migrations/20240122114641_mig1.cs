1881d227e3484753d7d18e58a11360e26984f928
FarasatN <farasatnovruzov@gmail.com>
2024-01-22 21:02:01 +0400

    Add project files.

---
+++ Migrations/20240122114641_mig1.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCoreShadowProperties.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.PersonId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    PostId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.PostId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "BlogId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookAuthors",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookAuthors", x => new { x.BookId, x.AuthorId });
+                    table.ForeignKey(
+                        name: "FK_BookAuthors_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookAuthors_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    AddressId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.AddressId);
+                    table.ForeignKey(
+                        name: "FK_Addresses_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Persons",
+                        principalColumn: "PersonId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_PersonId",
+                table: "Addresses",
+                column: "PersonId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAuthors_AuthorId",
+                table: "BookAuthors",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "BookAuthors");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,203 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace EfCoreShadowProperties
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var context = new AppDbContext();
+
+            //Shadow Properties - entity de olmayana ama efcore da olan ozelliklerdir
+            //Table da gosterilmesini istemediyimiz, emeliyyat aparmayacagimiz columnlar icin ist olunur
+            //shadow propertiesin deyerleri change trackir terefinden idare olunur
+
+            //biz eslinde Foreign Key columnlarinda bunu istifade etmisik
+            //var blogs = context.Blogs.Include(b => b.Posts)
+            //    .ToList();
+            //Console.WriteLine();
+
+            //Bir Entity uzerinde Shadow Properties yaratma1 ucun mutleq Fluent API ist. etmelisen
+            //Shadow Property yaratmaq ucun
+            //modelBuilder.Entity<Blog>()
+            //    .Property<DateTime>("CreatedDate");
+
+            //ChangeTracker ile access
+            //var blog = await context.Blogs.FirstAsync();
+            //var createdDate = context.Entry(blog).Property("CreatedDate");
+            //Console.WriteLine(createdDate.CurrentValue);//in memory value
+            //Console.WriteLine(createdDate.OriginalValue);
+            //createdDate.CurrentValue = DateTime.Now;
+            //await context.SaveChangesAsync();
+
+            //EF.Property uzerinden access - ozellikle LINQ sorgularinda
+            var blog = await context.Blogs.OrderBy(b => EF.Property<DateTime>(b, "CreatedDate")).ToListAsync();
+            var blog2 = await context.Blogs.Where(b => EF.Property<DateTime>(b, "CreatedDate").Year>2020).ToListAsync();
+            Console.WriteLine();
+
+
+        }
+
+        //---------------------------------
+
+
+
+        //1 to 1
+        public class Person
+        {
+            public int PersonId { get; set; }
+            public string name;
+            //yuxaridaki fieldi backin filed kimi ya getter setter ile, ya da attribute ile teyin etmek olar
+            //public string Name { get=>name.Substring(0,3); set=>name=value.Substring(0,3); }
+            //[BackingField(nameof(name))]
+
+            //Ya da Fluent API de conf. edilir
+            //public string Name { get; set; }
+
+            //Field-Only Properties
+            public string GetName()
+                => name;
+            public string SetName(string value)
+                => name = value;
+
+            // Navigation property for one-to-one relationship
+            public Address Address { get; set; }
+        }
+        public class Address
+        {
+            public int AddressId { get; set; }
+            public string Street { get; set; }
+            public string City { get; set; }
+
+            // Navigation property for one-to-one relationship
+            public Person Person { get; set; }
+            public int PersonId { get; set; }
+        }
+
+        //1 to n
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Title { get; set; }
+
+            // Navigation property for one-to-many relationship
+            public ICollection<Post> Posts { get; set; }
+        }
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Content { get; set; }
+
+            // Foreign key property for one-to-many relationship
+            public int BlogId { get; set; }//default conventionda bunu yazmaga ehtiyyac yoxdur, cunki bu shadow properties rolunu oynayir
+
+            // Navigation property for one-to-many relationship
+            public Blog Blog { get; set; }
+        }
+
+        //n to n
+        public class Book
+        {
+            public int BookId { get; set; }
+            public string Title { get; set; }
+
+            // Navigation property for the many-to-many relationship
+            public ICollection<BookAuthor> BookAuthors { get; set; }
+        }
+        public class Author
+        {
+            public int AuthorId { get; set; }
+            public string Name { get; set; }
+
+            // Navigation property for the many-to-many relationship
+            public ICollection<BookAuthor> BookAuthors { get; set; }
+        }
+
+        // Join entity for the many-to-many relationship
+        public class BookAuthor
+        {
+            public int BookId { get; set; }
+            public Book Book { get; set; }
+
+            public int AuthorId { get; set; }
+            public Author Author { get; set; }
+        }
+
+        public class AppDbContext : DbContext
+        {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EfCoreShadowProperties;Trusted_Connection=True;");
+            }
+
+            public DbSet<Person> Persons { get; set; }
+            public DbSet<Address> Addresses { get; set; }
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+            public DbSet<Book> Books { get; set; }
+            public DbSet<Author> Authors { get; set; }
+            public DbSet<BookAuthor> BookAuthors { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                //1 to 1
+                modelBuilder.Entity<Person>()
+                    //.Property(p => p.Name);
+                    //Field-Only Properties ucun HasField olmamalidir ve asagidaki kimi olacaq
+                    .Property(nameof(Person.name));
+                //.HasField(nameof(Person.name));//backing field in fluent api
+
+                modelBuilder.Entity<Person>()
+                .HasOne(p => p.Address)
+                .WithOne(a => a.Person)
+                .HasForeignKey<Address>(a => a.PersonId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+                //1 to many
+                modelBuilder.Entity<Blog>()
+                .HasMany(b => b.Posts)   // One Blog has many Posts
+                .WithOne(p => p.Blog)    // Each Post belongs to one Blog
+                .HasForeignKey(p => p.BlogId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+                //Shadow Property yaratmaq ucun
+                modelBuilder.Entity<Blog>()
+                    .Property<DateTime>("CreatedDate");
+
+                //many to many
+                modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+                modelBuilder.Entity<BookAuthor>()
+                    .HasOne(ba => ba.Book)
+                    .WithMany(b => b.BookAuthors)
+                    .HasForeignKey(ba => ba.BookId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                modelBuilder.Entity<BookAuthor>()
+                    .HasOne(ba => ba.Author)
+                    .WithMany(a => a.BookAuthors)
+                    .HasForeignKey(ba => ba.AuthorId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+            }
+
+        
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
